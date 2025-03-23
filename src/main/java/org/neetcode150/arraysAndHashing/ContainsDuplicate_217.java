6eb6ccd2571252cf0872354bd2a1d97c06c37b8b
Omkar Nate <nate.o@northeastern.edu>
2025-03-23 16:05:29 -0400

    Reformat

--- src/main/java/org/neetcode150/arraysAndHashing/ContainsDuplicate_217.java
+++ src/main/java/org/neetcode150/arraysAndHashing/ContainsDuplicate_217.java
@@ -4,17 +4,17 @@ import java.util.*;
 
 public class ContainsDuplicate_217 {
 
-/*
-    Problem Statement: If an array contains duplicate return false else return true
-    Intuition: Use sets to store all elements, if no duplicates are present, size will be different
-    Time Complexity: O(n)
-    Space Complexity: O(1)
- */
+    /*
+        Problem Statement: If an array contains duplicate return false else return true
+        Intuition: Use sets to store all elements, if no duplicates are present, size will be different
+        Time Complexity: O(n)
+        Space Complexity: O(1)
+     */
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> set = new HashSet<>();
 
-        for(int num:nums){
-            if(set.contains(num)){
+        for (int num : nums) {
+            if (set.contains(num)) {
                 return true;
             }
             set.add(num);
@@ -23,7 +23,7 @@ public class ContainsDuplicate_217 {
     }
 
     public static void main(String[] args) {
-        int[] nums = {1,2,3,4,5,1};
+        int[] nums = {1, 2, 3, 4, 5, 1};
         ContainsDuplicate_217 obj = new ContainsDuplicate_217();
         boolean ans = obj.containsDuplicate(nums);
 
