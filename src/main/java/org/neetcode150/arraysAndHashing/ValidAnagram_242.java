abafaff42097d0384d591553233830df582dde6e
Omkar Nate <nate.o@northeastern.edu>
2025-03-23 16:03:23 -0400

    Reformat

--- src/main/java/org/neetcode150/arraysAndHashing/ValidAnagram_242.java
+++ src/main/java/org/neetcode150/arraysAndHashing/ValidAnagram_242.java
@@ -2,24 +2,24 @@ package org.neetcode150.arraysAndHashing;
 
 public class ValidAnagram_242 {
 
-/*
-    Problem Statement: Two strings having exactly same characters (including their count)
-    Intuition: Use hashtable, store character counts with character ascii as key/index.
-    Time Complexity: O(n), where n is the length of the string, since both have same length
-    Space Complexity: O(1), constant space to store a-z alphabets
- */
+    /*
+        Problem Statement: Two strings having exactly same characters (including their count)
+        Intuition: Use hashtable, store character counts with character ascii as key/index.
+        Time Complexity: O(n), where n is the length of the string, since both have same length
+        Space Complexity: O(1), constant space to store a-z alphabets
+     */
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()) return false;
+        if (s.length() != t.length()) return false;
 
         int[] charCount = new int[26];
 
-        for(int i = 0; i < s.length(); i++){
-            charCount[s.charAt(i)-'a']++;
-            charCount[t.charAt(i)-'a']--;
+        for (int i = 0; i < s.length(); i++) {
+            charCount[s.charAt(i) - 'a']++;
+            charCount[t.charAt(i) - 'a']--;
         }
 
-        for(int count : charCount){
-            if(count!=0) return false;
+        for (int count : charCount) {
+            if (count != 0) return false;
         }
 
         return true;
